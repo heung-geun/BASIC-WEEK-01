493d282212ea64b579604ceee9f9fe4c3f05620a
yunhyeunggeun <gudrms121712@naver,com>
2024-11-21 15:58:20 +0900

    주석 제거

--- week2/Level3.js
+++ week2/Level3.js
@@ -7,10 +7,10 @@ const objArray = [
 ];
 
 // myFind 를 구현하여 arr.find 와 동일한 값이 나오도록 하기.
+
 function myFind(arr, callback) {
   for(let i = 0; i < arr.length; i++) {
     if(callback(arr[i])) {
-        return arr[i];
     }
   }
 }
@@ -24,44 +24,44 @@ const result2 = myFind(objArray, function (obj) {
 });
 console.log(result1 === result2); // true
 
-// // (2) findIndex 구현
+// (2) findIndex 구현
 
-// const objArray = [
-//   { name: "apple", price: 100 },
-//   { name: "banana", price: 200 },
-//   { name: "grape", price: 300 },
-// ];
+const objArray1 = [
+  { name: "apple", price: 100 },
+  { name: "banana", price: 200 },
+  { name: "grape", price: 300 },
+];
 
-// // myFindIndex 를 구현하여 arr.findIndex 와 동일한 값이 나오도록 하기.
-// function myFindIndex(arr, callback) {
-//   // (3) myFindIndex 구현
-// }
+// myFindIndex 를 구현하여 arr.findIndex 와 동일한 값이 나오도록 하기.
+function myFindIndex(arr, callback) {
+  // (3) myFindIndex 구현
+}
 
-// const result1 = objArray.findIndex(function (obj) {
-//   return obj.name === "banana";
-// });
+const result3 = objArray1.findIndex(function (obj) {
+  return obj.name === "banana";
+});
 
-// const result2 = myFind(objArray, function (obj) {
-//   return obj.name === "banana";
-// });
-// console.log(result1 === result2); // true
+const result4 = myFind(objArray1, function (obj) {
+  return obj.name === "banana";
+});
+console.log(result3 === result4); // true
 
-// // (4) filter 사용
+// (4) filter 사용
 
-// const objArray = [
-//   { name: "apple", price: 100 },
-//   { name: "banana", price: 200 },
-//   { name: "grape", price: 300 },
-// ];
+const objArray2 = [
+  { name: "apple", price: 100 },
+  { name: "banana", price: 200 },
+  { name: "grape", price: 300 },
+];
 
-// // filter 를 이용하여 price가 200 이상인 객체 filter
+// filter 를 이용하여 price가 200 이상인 객체 filter
 
-// // map 사용
+// map 사용
 
-// const objArray = [
-//   { name: "apple", price: 100 },
-//   { name: "banana", price: 200 },
-//   { name: "grape", price: 300 },
-// ];
+const objArray3 = [
+  { name: "apple", price: 100 },
+  { name: "banana", price: 200 },
+  { name: "grape", price: 300 },
+];
 
-// // map을 이용하여 price값만 모아둔 배열 만들기
+// map을 이용하여 price값만 모아둔 배열 만들기
