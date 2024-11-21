c2b4c0b4578e02f0712f7007edc4a9f770fe7d2a
yunhyeunggeun <gudrms121712@naver,com>
2024-11-21 13:11:49 +0900

    week 2 의 3-1 번 문제까지 완료

--- week2/Level2.js
+++ week2/Level2.js
@@ -8,7 +8,7 @@
 
 // // myForEach 를 구현하여 arr.forEach 와 동일한 값이 나오도록 하기.
 // function myForEach(arr, callback) {
-//   // (2) myForEach 구현
+//   // myForEach 구현
 // }
 
 // /**
@@ -24,7 +24,7 @@
 //   console.log(obj);
 // });
 
-// // (3) find 사용
+// // (2) find 사용
 
 // const objArray = [
 //   { name: "apple", price: 100 },
@@ -34,12 +34,16 @@
 
 // // find를 이용하여 name 이 grape 인 객체를 찾기
 
-// // (4) findIndex 사용
+// (3) findIndex 사용
 
-// const objArray = [
-//   { name: "apple", price: 100 },
-//   { name: "banana", price: 200 },
-//   { name: "grape", price: 300 },
-// ];
+const objArray = [
+  { name: "apple", price: 100 },
+  { name: "banana", price: 200 },
+  { name: "grape", price: 300 },
+];
 
-// // findIndex를 이용하여 name 이 apple 인 객체의 index 찾기
+objArray.findIndex(function(value) {
+    return value.name === "apple";
+})
+
+// findIndex를 이용하여 name 이 apple 인 객체의 index 찾기
--- week2/Level3.js
+++ week2/Level3.js
@@ -1,24 +1,28 @@
-// // (1) find 구현
+// (1) find 구현
 
-// const objArray = [
-//   { name: "apple", price: 100 },
-//   { name: "banana", price: 200 },
-//   { name: "grape", price: 300 },
-// ];
+const objArray = [
+  { name: "apple", price: 100 },
+  { name: "banana", price: 200 },
+  { name: "grape", price: 300 },
+];
 
-// // myFind 를 구현하여 arr.find 와 동일한 값이 나오도록 하기.
-// function myFind(arr, callback) {
-//   // myFind 구현
-// }
+// myFind 를 구현하여 arr.find 와 동일한 값이 나오도록 하기.
+function myFind(arr, callback) {
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+        return arr[i];
+    }
+  }
+}
 
-// const result1 = objArray.find(function (obj) {
-//   return obj.name === "banana";
-// });
+const result1 = objArray.find(function (obj) {
+  return obj.name === "banana";
+});
 
-// const result2 = myFind(objArray, function (obj) {
-//   return obj.name === "banana";
-// });
-// console.log(result1 === result2); // true
+const result2 = myFind(objArray, function (obj) {
+  return obj.name === "banana";
+});
+console.log(result1 === result2); // true
 
 // // (2) findIndex 구현
 
