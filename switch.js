6adf33aeb3d88ddb0df9dbac5e7f7b8b5789d9ad
yunhyeunggeun <gudrms121712@naver,com>
2024-11-21 10:19:59 +0900

    스위치문 연습용 파일

---
+++ switch.js
@@ -0,0 +1,51 @@
+import readlineSync from "readline-sync";
+
+// switch case 문  break 대신 return으로 나오기
+
+// function calculator() {
+//     console.log("1 ~ 4 중 선택해주세요!");
+//     const operator = readlineSync.question("==> ");
+  
+//     switch (operator) {
+//       case "1":
+//         return console.log('1 입니다.');
+//       case "2":
+//         return console.log('2 입니다.');
+//       case "3":
+//         console.log('3 입니다.');
+//         break;
+//       case "4":
+//         return console.log('4 입니다.');
+//       default:
+//         console.log("연산자를 잘못 입력하셨습니다.");
+//         calculator()
+//         break;
+//     }
+//   }
+
+//   calculator()
+
+
+// 연습용 ㅎㅎ
+
+// function calculator() {
+//     console.log("1 ~ 4 중 선택해주세요!");
+//     const operator = readlineSync.question("==> ");
+  
+//     switch (operator) {
+//       case "1":
+//         console.log('1 입니다.');
+//       case "2":
+//         return console.log('2 입니다.');
+//       case "3":
+//         console.log('3 입니다.');
+//         break;
+//       case "4":
+//         console.log('4 입니다.');
+//       default:
+//         console.log("연산자를 잘못 입력하셨습니다.");
+//         calculator()
+//         break;
+//     }
+//   }
+// calculator()
