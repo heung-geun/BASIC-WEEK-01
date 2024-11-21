f73d7fe0f3846cd98ab5db74b2f2c37b401afea9
yunhyeunggeun <gudrms121712@naver,com>
2024-11-21 23:20:13 +0900

    2래벨 문제 forEach 구현하기 문제 추가

--- week2/Level2.js
+++ week2/Level2.js
@@ -1,48 +1,51 @@
-// // (1) forEach 구현
+// (1) forEach 구현
 
-// const objArray = [
-//   { name: "apple", price: 100 },
-//   { name: "banana", price: 200 },
-//   { name: "grape", price: 300 },
-// ];
-
-// // myForEach 를 구현하여 arr.forEach 와 동일한 값이 나오도록 하기.
-// function myForEach(arr, callback) {
-//   // myForEach 구현
-// }
-
-// /**
-//   { name: 'apple', price: 100 }
-//   { name: 'banana', price: 200 }
-//   { name: 'grape', price: 300 }
-//    */
-// objArray.forEach(function (obj) {
-//   console.log(obj);
-// });
-
-// myForEach(objArray, function (obj) {
-//   console.log(obj);
-// });
-
-// // (2) find 사용
-
-// const objArray = [
-//   { name: "apple", price: 100 },
-//   { name: "banana", price: 200 },
-//   { name: "grape", price: 300 },
-// ];
-
-// // find를 이용하여 name 이 grape 인 객체를 찾기
-
-// (3) findIndex 사용
-
-const objArray = [
+const objArray1 = [
   { name: "apple", price: 100 },
   { name: "banana", price: 200 },
   { name: "grape", price: 300 },
 ];
 
-objArray.findIndex(function(value) {
+// myForEach 를 구현하여 arr.forEach 와 동일한 값이 나오도록 하기.
+function myForEach(arr, callback) {
+  for(let i = 0; i < arr.length; i++) {
+    callback(arr[i]);
+  }
+}
+
+/**
+  { name: 'apple', price: 100 }
+  { name: 'banana', price: 200 }
+  { name: 'grape', price: 300 }
+   */
+objArray1.forEach(function (obj) {
+  console.log(obj);
+});
+
+myForEach(objArray1, function (obj) {
+  console.log(obj);
+});
+console.log('---------------------')
+
+// (2) find 사용
+
+const objArray2 = [
+  { name: "apple", price: 100 },
+  { name: "banana", price: 200 },
+  { name: "grape", price: 300 },
+];
+
+// find를 이용하여 name 이 grape 인 객체를 찾기
+
+// (3) findIndex 사용
+
+const objArray3 = [
+  { name: "apple", price: 100 },
+  { name: "banana", price: 200 },
+  { name: "grape", price: 300 },
+];
+
+objArray3.findIndex(function(value) {
     return value.name === "apple";
 })
 
