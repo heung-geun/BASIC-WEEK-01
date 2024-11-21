f73a98b82b7429328dcd870da5e7d205fbdd4532
yunhyeunggeun <gudrms121712@naver,com>
2024-11-21 12:07:21 +0900

    week 별 폴더 생성 및 2주차 문제 추가

---
+++ week2/Level2.js
@@ -0,0 +1,45 @@
+// // (1) forEach 구현
+
+// const objArray = [
+//   { name: "apple", price: 100 },
+//   { name: "banana", price: 200 },
+//   { name: "grape", price: 300 },
+// ];
+
+// // myForEach 를 구현하여 arr.forEach 와 동일한 값이 나오도록 하기.
+// function myForEach(arr, callback) {
+//   // (2) myForEach 구현
+// }
+
+// /**
+//   { name: 'apple', price: 100 }
+//   { name: 'banana', price: 200 }
+//   { name: 'grape', price: 300 }
+//    */
+// objArray.forEach(function (obj) {
+//   console.log(obj);
+// });
+
+// myForEach(objArray, function (obj) {
+//   console.log(obj);
+// });
+
+// // (3) find 사용
+
+// const objArray = [
+//   { name: "apple", price: 100 },
+//   { name: "banana", price: 200 },
+//   { name: "grape", price: 300 },
+// ];
+
+// // find를 이용하여 name 이 grape 인 객체를 찾기
+
+// // (4) findIndex 사용
+
+// const objArray = [
+//   { name: "apple", price: 100 },
+//   { name: "banana", price: 200 },
+//   { name: "grape", price: 300 },
+// ];
+
+// // findIndex를 이용하여 name 이 apple 인 객체의 index 찾기
